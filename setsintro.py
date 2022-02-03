ccdbf1bf446420eb8c1793f3e87e00255815a465
peram-vignesh <90543677+peram-vignesh@users.noreply.github.com>
2022-02-03 21:08:57 +0530

    3/2/22

---
+++ setsintro.py
@@ -0,0 +1,7 @@
+set1=set(input('Enter the first set'))
+set2=set(input('Enter the second set'))
+print("Union of E and N is",set1| set2)
+print("Intersection of E and N is",set1 & set2)
+print("Difference of E and N is",set1 - set2)
+print("Symmetric difference of E and N is",set1 ^ set2)
+
